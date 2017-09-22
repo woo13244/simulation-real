4044de0b5597406a0ac15d40dd710c4d1937b324
Skyler Riley <riley.skyler@gmail.com>
2017-09-22 09:56:11 -0700

    Added css and more

--- simulation-3/src/components/Login/Login.js
+++ simulation-3/src/components/Login/Login.js
@@ -1,1 +1,21 @@
-ffadsf
+import React, {Component} from 'react';
+import logo from './logo.png';
+import './Login.css'
+
+class Login extends Component {
+    render() {
+      return (
+    <div className="auth">
+        <div className='splash-screen'>
+          <img className='' src={logo}/>
+          <span className='name'>Helo</span>
+          <button>Login / Register</button>
+        </div>
+      </div>
+
+      );
+    }
+  }
+  
+  export default Login;
+  
